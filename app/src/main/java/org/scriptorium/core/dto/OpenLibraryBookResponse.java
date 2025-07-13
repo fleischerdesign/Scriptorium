f839ae72687113cad6766014bafab78056a6719c
Philipp Fleischer <philipp@fleischer.design>
2025-07-14 01:29:32 +0200

    Ignore unknown properties in OpenLibraryBook

--- app/src/main/java/org/scriptorium/core/dto/OpenLibraryBookResponse.java
+++ app/src/main/java/org/scriptorium/core/dto/OpenLibraryBookResponse.java
@@ -1,8 +1,10 @@
 package org.scriptorium.core.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OpenLibraryBookResponse {
     @JsonProperty("numFound")
     private int numFound;
